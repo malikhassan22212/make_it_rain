7564ee8887e6c6a1edf4bec2d44595ab8270edb9
Hassan Rauf <hassanrauf@ncloud.hk>
2019-11-11 11:05:26 +0500

    make it rain 93

--- lib/ui/MakeitRain.dart
+++ lib/ui/MakeitRain.dart
@@ -4,12 +4,21 @@ class MakeItRain extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-
     return new MakeItRainState();
   }
 }
 
 class MakeItRainState extends State<MakeItRain> {
+  int _moneyCounter = 0;
+
+  void _rainMoney() {
+    //set state is called each time to update The UI
+    setState(() {
+      _moneyCounter = _moneyCounter + 100;
+      print(_moneyCounter);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -26,7 +35,7 @@ class MakeItRainState extends State<MakeItRain> {
               child: new Text(
                 "Rich",
                 style: new TextStyle(
-                    color: Colors.grey,
+                    color: Colors.blue,
                     fontWeight: FontWeight.w800,
                     fontSize: 29.9),
               ),
@@ -34,9 +43,9 @@ class MakeItRainState extends State<MakeItRain> {
             new Expanded(
                 child: new Center(
               child: new Text(
-                "Money",
+                '\$$_moneyCounter',
                 style: new TextStyle(
-                    color: Colors.grey,
+                    color: Colors.blue,
                     fontSize: 46.9,
                     fontWeight: FontWeight.w800),
               ),
@@ -44,9 +53,9 @@ class MakeItRainState extends State<MakeItRain> {
             new Expanded(
                 child: new Center(
               child: new FlatButton(
-                  color: Colors.grey,
-                  textColor: Colors.lightGreen,
-                  onPressed: () => debugPrint("hey"),
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  onPressed: () => _rainMoney(),
                   child: new Text(
                     "Let it Rain",
                     style: new TextStyle(),
